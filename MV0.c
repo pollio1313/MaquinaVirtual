3f17c706e8b9ec43b3ff1b02510427a962d7f60e
Junior Peguero <juniorpeguero2013@gmail.com>
2026-09-13 12:52:35 -0300

    Cambios en la Lectura del archivo y en el main

--- MV0.c
+++ MV0.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 struct operandos {
     int cod;
     char *tipo;
-    int cantBytes; 
+    int cantBytes;
 };
 
 const struct operandos tablaOperandos[] = {
@@ -17,7 +18,7 @@ struct Instruccion {
     char *nombre;       //esta sirve por ahrao, pero en realidad tendria que ser punteros a funciones con cada metodo
     unsigned char codigo;
     int cantOP;
-};                          
+};
 
 const struct Instruccion tablaInstrucciones[] = {
     {"SYS",  0x00, 1},
@@ -55,34 +56,39 @@ const struct Instruccion tablaInstrucciones[] = {
 };
 
 char posReg(unsigned char pos){             //totalmente incompleta, pero para acordarme, habria q hacerlo como la de instrucciones
-    
+
 }
 
-
-void Lectura(unsigned char MemoriaPrincipal[][4]){
+// Lectura
+//Modifique la lectura para no tener 'prueba1.vmx' hardcodeado
+int CargarArchivo(const char* filename, unsigned char* memoria) {
+    int estado_carga = 0;
+    int bytes_leidos;
     FILE *archivoVMX;
-    int i=0;
 
-    archivoVMX=fopen("prueba1.vmx","r");        //abro el archivo vmx
-    if (archivoVMX==NULL){
-        printf("NO SE ABRIO CAPO");
-    }
-    else{
-        while(fread(&MemoriaPrincipal[i][3],sizeof(unsigned char),1,archivoVMX)==1){
-            MemoriaPrincipal[i][2]=MemoriaPrincipal[i][1]=MemoriaPrincipal[i][0]=0;
-            i++;
-        }
+    archivoVMX = fopen(filename, "rb");
+
+    if (archivoVMX == NULL) {
+        printf("Error: No se pudo abrir el archivo %s\n", filename);
+    } else {
+        bytes_leidos = fread(memoria, sizeof(unsigned char), 16384, archivoVMX);
         fclose(archivoVMX);
+        printf("Se cargaron %d bytes en la memoria.\n", bytes_leidos);
+        estado_carga = 1;
     }
+
+    return estado_carga;
 }
 
+//Lo unico que falta adaptar a este arreglo de 1 dimensión es la función MostrarCodigo
+/*
 void MostrarCodigo(unsigned char MemoriaPrincipal[][4]){
     int i,j,cant;
     unsigned char codOperacion,OperandoA,OperandoB,valorOPA[3]={0},valorOPB[3]={0};
     for( i=0;i<=4;i++){
         printf("%c",MemoriaPrincipal[i][3]);            //primeros 5 bytes el VMX
     }
-    
+
     printf(" %02X",MemoriaPrincipal[i][3]);               //muestra la version
     i+=2;
     printf(" %02X%02X \n",MemoriaPrincipal[i-1][3],MemoriaPrincipal[i][3]);
@@ -92,50 +98,54 @@ void MostrarCodigo(unsigned char MemoriaPrincipal[][4]){
         codOperacion= MemoriaPrincipal[j][3] & 0x1F;                            //aplico una mascara, para sacarle los ultimos 5 bits y asi tenes el codigo de operacion
         cant =tablaInstrucciones[codOperacion].cantOP;
         if (cant==1){
-            OperandoA=MemoriaPrincipal[j][3] >>6; 
+            OperandoA=MemoriaPrincipal[j][3] >>6;
         }
         else if (cant==2){                   //se fija cuantos bytes chupa cada operando
             OperandoB=MemoriaPrincipal[j][3] >>6;
             OperandoA=(MemoriaPrincipal[j][3] >>4) & 0x03;
         }
         else if (cant==0){                   //esta linea esta de mas, porq ya deberian valer 0 de antes
-            OperandoA=OperandoB=0;    
+            OperandoA=OperandoB=0;
         }
-        
+
         for(int q=0;q<OperandoB;q++){                 //while apra consumir los valores de operandos, si es 0 sigeun de largo ,uso operandoB y no cantBytes porq valen lo mismo
             j++;                                         //parece qprimero viene el byte mas signfiquitaivo
-            valorOPB[q]=MemoriaPrincipal[j][3]; 
-            //printf("      valor[%d]=%02X q%d  ",q,valorOPB[q],q);           
+            valorOPB[q]=MemoriaPrincipal[j][3];
+            //printf("      valor[%d]=%02X q%d  ",q,valorOPB[q],q);
         }
         for(int q=0;q<OperandoA;q++){                 //while apra consumir los valores de operandos, si es 0 sigeun de largo ,uso operandoB y no cantBytes porq valen lo mismo
             j++;
-            valorOPA[q]=MemoriaPrincipal[j][3];            
+            valorOPA[q]=MemoriaPrincipal[j][3];
         }
         printf("\n %s",tablaInstrucciones[codOperacion].nombre);
         printf("  %02X%02X%02X,%02X%02X%02X",valorOPA[0],valorOPA[1],valorOPA[2],valorOPB[0],valorOPB[1],valorOPB[2]);      //esta mierda es de prueba, hay q hacer codicional de la cantidad de bytes del operando
-        
+
         j++;
     }
     printf("\nj:%d",j);
 }
+*/
 
-
-void main(int argc, char argv[]){
-    //con [][4] estarian separas byte a byte, sino 32 y estarian bit a bit
-    int Registros[17][4];                      //para la proxima es de 32x32, ahora solo usamos 17//deberian ser booleanos?
-    unsigned char MemoriaPrincipal[4096][4];            //mismo   16384 bytes tomados de a 4 
-    int TablaSegmentos[8][4];                  //0 cs, 1 ds,
-
+int main(int argc, char *argv[]) {
+    int codigo_salida = 0;
+    int mostrar_disassembler = 0;
+    const char* filename;
+    int Registros[32] = {0};
+    unsigned char MemoriaPrincipal[16384] = {0}; //unsigned char ya es exactamente 1 byte (8 bits)
+    //se nos iba a hacer un quilombo para calcular las direcciones fisicas
+    struct {
+        unsigned short base;
+        unsigned short size;
+    } TablaSegmentos[8];
 
     // argv[0] = nombre del propio programa (ej: "./vmx"), siempre está
     // argv[1] = filename.vmx (obligatorio)
     // argv[2] = "-d" (opcional)
     if (argc < 2) {
         printf("Uso: vmx filename.vmx [-d]\n");
-    }
-    else{
+        codigo_salida = 1;
+    } else {
         filename = argv[1];
-        int mostrar_disassembler = 0;
 
         if (argc >= 3 && strcmp(argv[2], "-d") == 0) {
             mostrar_disassembler = 1;
@@ -144,13 +154,12 @@ void main(int argc, char argv[]){
         printf("Archivo a ejecutar: %s\n", filename);
         printf("Modo disassembler: %s\n", mostrar_disassembler ? "SI" : "NO");
 
-        // Acá seguiría: abrir el archivo, leer la cabecera, cargar en memoria,
-        // ejecutar (y si mostrar_disassembler, imprimir el disassembler)
+        if (CargarArchivo(filename, MemoriaPrincipal) == 0) {
+            codigo_salida = 1;
+        } else {
+            /* Aca iria la llamada a MostrarCodigo o EjecutarMaquina */
+        }
     }
 
-
-    Lectura(MemoriaPrincipal);
-    MostrarCodigo(MemoriaPrincipal);
-    
-
+    return codigo_salida;
 }
